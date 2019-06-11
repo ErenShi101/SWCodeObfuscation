a2fb07c1193fc207a265f1c10f47a31d74ef7016
Eren Shi <eren.shi@nextlabs.com>
2019-06-11 14:56:55 +0800

    first commit

---
+++ SWCodeObfuscation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SWCodeObfuscation
+//
+//  Created by Eren on 2019/6/11.
+//  Copyright © 2019 skyline. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
